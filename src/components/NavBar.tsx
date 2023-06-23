18fe2f35735fec2dd7736053c99f8338ed0ebc87
Bimal Saggu <bimalsaggu28@gmail..com>
2023-06-23 22:09:27 +0530

    Reduce aside column and logo size

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ import SearchInput from "./SearchInput";
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px"></Image>
+      <Image src={logo} boxSize="50px"></Image>
       <SearchInput></SearchInput>
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
