f316dd37acd6eaa29d5592adcd48002e1ec312f8
Bimal Saggu <bimalsaggu28@gmail..com>
2023-07-25 15:41:00 +0530

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,10 +1,13 @@
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Game[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform; }[];
