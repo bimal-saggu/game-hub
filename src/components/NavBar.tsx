bae37349ded486b5d01b0d59d00677298c2f9e4a
Bimal Saggu <bimalsaggu28@gmail..com>
2023-07-25 18:24:15 +0530

    Fix the layout and navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,14 @@ import logo from "../assets/logo.webp";
 import ColoModeSwitch from "./ColorModeSwitch";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="50px"></Image>
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="contain" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
